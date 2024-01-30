3c56dcd6fd3ff96f8e2a9d90834dfe4e26c44800
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-01-30 21:54:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  //Add your code here
+  let totalSum = 0;
+  const prices = document.querySelectorAll(".price");
+  prices.forEach((e) => {
+    totalSum += Number(e.innerText);
+  });
+
+  const table = document.querySelector("table");
+  const row = document.createElement("tr");
+  const cell1 = document.createElement("td");
+  const cell2 = document.createElement("td");
+  cell1.innerText = "Total Price";
+  cell2.innerText = totalSum;
+  row.appendChild(cell1);
+  row.appendChild(cell2);
+  table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
